60a55e9ea04d28cc30a219af88d3ab75ceab052e
shahbazsingh <shahbaz.nanda@mail.utoronto.ca>
2023-11-04 18:03:02 -0400

    choooot

--- Transcript_Test.py
+++ Transcript_Test.py
@@ -7,10 +7,15 @@ with open('major_crimes_smaller.csv', newline='', encoding='utf-8') as csv_file:
 
     # Read the header row
     header = next(csv_reader)
-    print(header)
-    # Initialize a list to store dictionaries
-    data_list = []
-
-    # Iterate through the rows and create dictionaries
+    dic = {}
     for row in csv_reader:
-        print(row)
+        longe = row[4]
+        lat = row[5]
+        long_lat = (longe, lat)
+        sub_dict = {}
+        sub_dict[header[0]] = row[0]
+        # sub_dict[header[1]] = row[1]
+        sub_dict[header[2]] = row[2]
+        sub_dict[header[3]] = row[3]
+        dic[long_lat] = sub_dict
+    print(dic)
