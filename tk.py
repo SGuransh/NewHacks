dedf73dc763e93315ea5a72daa36fd9b989ec221
shahbazsingh <shahbaz.nanda@mail.utoronto.ca>
2023-11-05 04:07:50 -0500

    saving

--- tk.py
+++ tk.py
@@ -9,7 +9,6 @@ class StartEndBoxes(tk.CTkFrame):
     map_widget = None
     def __init__(self, parent, *args, **kwargs):
         super().__init__(parent, *args, **kwargs)
-
         self.start_label = tk.CTkLabel(self, text="Start:", font=("Arial", 14))
         self.start_entry = tk.CTkEntry(self, width=200)
 
